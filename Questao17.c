e91d1cae7a7cbae0d43b750a6305d9304fa28768
Henrique Hideaki Koga <hideakikoga.hk@gmail.com>
2021-02-18 01:38:20 -0300

    Lista de Ponteiros

---
+++ Questao17.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void somavetor(float *vetor1,float *vetor2, float *vetor_soma, int tam){
+    int i,j;
+    printf("A soma dos vetores eh: \n");
+    for (i=0; i<tam; i++) {
+        vetor_soma[i] = vetor1[i] + vetor2[i];
+    }
+    for (j=0; j<tam; j++) {
+        printf("%f \n",vetor_soma[j]);
+    }
+}
+
+int main(){
+    int tam;
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d",&tam);
+    float num1,num2;
+    float vet1[tam],vet2[tam],vet3[tam];
+    
+    printf("Digite os elementos do primeiro vetor: \n");
+    for (int i=0; i<tam; i++) {
+        scanf("%f",&num1);
+        vet1[i] = num1;
+    }
+    
+    printf("Digite os elementos do segundo vetor: \n");
+    for (int j=0; j<tam; j++) {
+        scanf("%f",&num2);
+        vet2[j] = num2;
+    }
+    
+    
+    somavetor(&vet1, &vet2, &vet3,tam);
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
